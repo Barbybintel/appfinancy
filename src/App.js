17de3505dcfb70546f69a87b8e7415f33c22dd84
Android Studio <devandroid@ort>
2022-08-31 10:43:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./styles/app.scss"
+
+import PageContainer from "./componentes/containers/PageContainer";
+import Navbar from "./componentes/NavBar/NavBar";
+import MobileNavBar from "./componentes/NavBar/MobileNavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PageContainer>
+        <div className="mobileMenu">
+          <MobileNavBar/>
+        </div>
+        <Navbar/>
+      </PageContainer>
     </div>
   );
 }
 
 export default App;
+
